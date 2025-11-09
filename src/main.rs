fa4389eebba7c891469998848517a982498dae60
cezirer <retinasliver@163.com>
2025-11-09 21:40:25 +0800

    2nd practice

--- src/main.rs
+++ src/main.rs
@@ -5,4 +5,8 @@ fn main() {
     // TODO: Uncomment the code below to pass the first stage
     print!("$ ");
     io::stdout().flush().unwrap();
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    print!("{}: command not found", input.trim());
+    io::stdout().flush().unwrap();
 }
