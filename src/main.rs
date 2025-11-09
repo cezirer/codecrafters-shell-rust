72a1ec9d3272b6348b4afc943e986ff6b81475a9
cezirer <retinasliver@163.com>
2025-11-09 22:43:57 +0800

    REPL fix

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,12 @@ use std::io::{self, Write};
 
 fn main() {
     // TODO: Uncomment the code below to pass the first stage
-    while true {
+    loop {
         print!("$ ");
         io::stdout().flush().unwrap();
         let mut input = String::new();
         io::stdin().read_line(&mut input).unwrap();
-        print!("{}: command not found", input.trim());
+        print!("{}: command not found\n", input.trim());
         io::stdout().flush().unwrap();
     }
 }
