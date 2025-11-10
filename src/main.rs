ffc65a77b21935e4140ee3a2967f9f03a81ee318
cezirer <retinasliver@163.com>
2025-11-10 21:44:47 +0800

    exit

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::process::exit;
 
 fn main() {
     // TODO: Uncomment the code below to pass the first stage
@@ -8,7 +9,13 @@ fn main() {
         io::stdout().flush().unwrap();
         let mut input = String::new();
         io::stdin().read_line(&mut input).unwrap();
-        print!("{}: command not found\n", input.trim());
-        io::stdout().flush().unwrap();
+        let input = input.split(" ").collect::<Vec<&str>>()[0];
+        match input {
+            "exit" => exit(0),
+            _ => {
+                print!("{}: command not found\n", input.trim());
+                io::stdout().flush().unwrap();
+            }
+        }
     }
 }
