2f11dacd22b93c5ffdef7ce5139a957dde7554be
cezirer <retinasliver@163.com>
2025-12-03 23:32:46 +0800

    add hoe dir recognized

--- src/builtins/cd.rs
+++ src/builtins/cd.rs
@@ -1,8 +1,14 @@
 use std::env;
 use std::path::Path;
 use std::process::ExitStatus;
+
 pub fn execute(args: &[String]) -> Result<ExitStatus, String> {
     let path_arg = args.get(0).ok_or("args error".to_string())?;
+    if path_arg.contains("~") {
+        let home_path = env::home_dir().ok_or("failed to get home path".to_string())?;
+        env::set_current_dir(home_path).map_err(|e| e.to_string())?;
+        return Ok(ExitStatus::default());
+    }
     let path = Path::new(path_arg);
     env::set_current_dir(path).map_err(|e| {
         let e_str = e.to_string();
@@ -11,7 +17,7 @@ pub fn execute(args: &[String]) -> Result<ExitStatus, String> {
         }
         e_str
     })?;
-    // println!("{:?}", env::current_dir());
+    let _ = env::current_dir();
     Ok(ExitStatus::default())
 }
 
@@ -21,7 +27,7 @@ mod tests {
     use super::*;
     #[test]
     fn test() {
-        let _ = execute(&[r"..".to_string()]);
+        let _ = execute(&[r"~".to_string()]);
     }
     #[test]
     fn test_no_such_output() {
